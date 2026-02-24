e4383f2799012cb6dd17d57c4b3bb7a47428686d
joguerrero10 <joelg1014@hotmail.com>
2026-02-23 19:00:45 -0500

    Cree el proyecto de vite y explico como trabajar con ramas en git

---
+++ vite-project/src/joel/numbers-positive.ts
@@ -0,0 +1,9 @@
+let number: number = -5;
+
+if (number > 0) {
+  console.log('Numero positivo');
+} else if (number < 0) {
+  console.log('Numero negativo');
+} else {
+  console.log('El numero es 0');
+}
